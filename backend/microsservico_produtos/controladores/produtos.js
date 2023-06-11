c583c38768a8e17127ba0f6ced7971e3c608886f
Lucas Roberto Silveira <lucaslhp.lr@gmail.com>
2023-06-11 16:04:58 -0300

    atualizacao mss de produtos

--- backend/microsservico_produtos/controladores/produtos.js
+++ backend/microsservico_produtos/controladores/produtos.js
@@ -1,4 +1,4 @@
-const {getTodosProdutos} = require ("../servicos/produtos")
+const {getTodosProdutos, getProdutosPorId, modificaProduto, deletaProdutoPorId} = require ("../servicos/produtos")
 
 function getProdutos (req, res) {
   try {
@@ -10,6 +10,44 @@ function getProdutos (req, res) {
   }
 }
 
+function getProduto (req, res) {
+  try {
+    const id = req.params.id
+    const produto = getProdutosPorId(id)
+    res.send(produto)
+  } catch (error) {
+    res.status(500)
+    res.send(error.message)
+  }
+}
+
+function patchProdutos (req, res) {
+  try {
+    const id = req.params.id
+    const body = req.body
+
+    modificaProduto(body, id)
+    res.send("produto alterado com sucesso")
+  } catch (error) {
+    res.status(500)
+    res.send(error.message)
+  }
+}
+
+function deleteProdutos (req, res) {
+  try {
+    const id = req.params.id
+    deletaProdutoPorId(id)
+    res.send("produto excluído do carrinho")
+  } catch (error) {
+    res.status(500)
+    res.send(error.message)
+  }
+}
+
 module.exports = {
-  getProdutos
+  getProdutos,
+  getProduto,
+  patchProdutos, 
+  deleteProdutos 
 }
--- backend/microsservico_produtos/servicos/produtos.js
+++ backend/microsservico_produtos/servicos/produtos.js
@@ -4,6 +4,34 @@ function  getTodosProdutos() {
   return JSON.parse(fs.readFileSync("produtos.json"))
 }
 
+function getProdutosPorId(id) {
+  const produtos = JSON.parse(fs.readFileSync("produtos.json"))
+
+  const produtoFiltrado = produtos.filter(produto => produto.id === id) [0]
+  return produtoFiltrado
+}
+
+function modificaProduto(modificacoes, id) {
+  let produtosAtuais = JSON.parse(fs.readFileSync("produtos.json"))
+  const indiceModificado = produtosAtuais.findIndex(produto => produto.id === id)
+
+  const produtoAlterado = { ...produtosAtuais[indiceModificado], ...modificacoes}
+
+  produtosAtuais[indiceModificado] = produtoAlterado
+
+  fs.writeFileSync("produtos.json", JSON.stringify(produtosAtuais))
+}
+
+function deletaProdutoPorId(id) {
+  const produtos = JSON.parse(fs.readFileSync("produtos.json"))
+
+  const produtosFiltrados = produtos.filter(produto => produto.id !== id)
+  fs.writeFileSync("produtos.json", JSON.stringify(produtosFiltrados))
+}
+
 module.exports = {
-  getTodosProdutos
+  getTodosProdutos,
+  getProdutosPorId,
+  modificaProduto,
+  deletaProdutoPorId
 }
